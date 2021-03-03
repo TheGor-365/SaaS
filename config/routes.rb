affb44cd0e3ae2f4da4662ed62b335db43fa4952
Gor <7495500@gmail.com>
2021-03-03 12:51:39 +0300

    Add registrations_controller for redirect with sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount StripeEvent::Engine, at: '/webhooks/stripe'
 
-  devise_for :accounts
+  devise_for :accounts, controllers: { registrations: "registrations" }
 
   get '/plans' => 'public#plans'
 
