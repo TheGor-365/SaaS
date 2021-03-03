e5936729a13320afd67c333aee75a7d3ebadc470
Gor <7495500@gmail.com>
2021-03-03 11:13:02 +0300

    Add plans page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,16 @@
 class PublicController < ApplicationController
+
   def home
   end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def plans
+    render 'accounts/plans'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  mount StripeEvent::Engine, at: '/webhooks/stripe' 
-
+  mount StripeEvent::Engine, at: '/webhooks/stripe'
 
   devise_for :accounts
+
+  get '/plans' => 'public#plans'
+
   root to: 'public#home'
 
 end
